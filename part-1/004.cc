f90c3370a15324d2fb5c55fa2c18659a5f6834a5
chfanghr <chfanghr@gmail.com>
2019-09-19 00:31:59 +0800

    fix minor syntax problem; add MIT license

--- part-1/004.cc
+++ part-1/004.cc
@@ -7,7 +7,7 @@
 #include <algorithm>
 
 auto main() -> int {
-  std::array<int64_t, 3> numbers = std::array<int64_t, 3>();
+  auto numbers = std::array<int64_t, 3>();
   std::cin >> numbers[0] >> numbers[1] >> numbers[2];
   std::sort(std::begin(numbers), std::end(numbers));
   std::cout << numbers[2] << std::endl;
