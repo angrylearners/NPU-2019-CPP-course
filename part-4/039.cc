72c7d2309b47c544de589121cf627abcd33770cb
chfanghr <chfanghr@gmail.com>
2019-09-24 14:31:44 +0800

    finish part4

--- part-4/039.cc
+++ part-4/039.cc
@@ -1,6 +1,39 @@
 //
 // Created by 31838 on 9/23/2019.
 //
-auto main() -> int {
 
+#include <queue>
+#include <iostream>
+#include "common.h"
+
+auto main() -> int {
+  auto num = Input<uint64_t>(std::cin);
+  auto shift = Input<int64_t>(std::cin);
+  
+  std::vector<char> digits{};
+  
+  while (num > 0) {
+    auto ch = (char) ((num % 10) + '0');
+    digits.push_back(ch);
+    num /= 10;
+  }
+  
+  if (shift < 0)
+    for (uint64_t i = 0; i < -1 * shift; i++) {
+      auto front = digits.front();
+      digits.erase(digits.begin());
+      digits.push_back(front);
+    }
+  else if (shift > 0)
+    for (uint64_t i = 0; i < shift; i++) {
+      auto back = digits.back();
+      digits.erase(digits.end() - 1);
+      digits.insert(digits.begin(), back);
+    }
+  
+  for (auto v:digits)
+    std::cout << v;
+  std::cout << std::endl;
+  return EXIT_SUCCESS;
 }
+
--- part-4/040.cc
+++ part-4/040.cc
@@ -1,6 +1,19 @@
 //
 // Created by 31838 on 9/23/2019.
 //
-auto main() -> int {
 
+#include <string>
+#include <iostream>
+#include <vector>
+
+auto main() -> int {
+  std::string original{};
+  std::cin >> original;
+  std::vector<char> reversed{};
+  for (auto ch:original)
+    reversed.insert(reversed.begin(), ch);
+  for (auto ch:reversed)
+    std::cout << ch;
+  std::cout << std::endl;
+  return EXIT_SUCCESS;
 }
