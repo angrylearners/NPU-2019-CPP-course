0c526b1788f1922dd6af43e7534b3a4882ed397a
方泓睿 <40087668+chfanghr@users.noreply.github.com>
2019-10-15 19:16:09 +0800

    Update 022.cc

--- part-3/022.cc
+++ part-3/022.cc
@@ -2,23 +2,6 @@
 //// Created by 31838 on 9/23/2019.
 ////
 //
-////#include "common.h"
-//#include <iostream>
-//#include <vector>
-//#include <string>
-//
-//auto main() -> int {
-//  auto n = Input<uint32_t>(std::cin);
-//  std::vector<uint32_t> res{};
-//  for (uint32_t i = 0; i <= n; i++) {
-//    auto numStr = std::to_string(i);
-//    if (numStr.find('7' + '0') != std::string::npos || i % 7 == 0)
-//      res.push_back(i);
-//  }
-//
-//  std::cout << res << std::endl;
-//  return EXIT_SUCCESS;
-//}
 
 #include <iostream>
 #include <string>
@@ -28,7 +11,7 @@ auto main() -> int {
   auto n = Input<uint32_t>(std::cin);
   std::vector<uint32_t> res{};
   for (uint32_t i = 0; i <= n; i++) {
-    if (std::to_string(i).find(7 + '0') != std::string::npos || i % 7 == 0)
+    if (std::to_string(i).find('7') != std::string::npos || i % 7 == 0)
       res.push_back(i);
   }
   std::cout << res << std::endl;
